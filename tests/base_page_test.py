44a9659a4c194a45988ae732fead1103540d4351
bugaev <bugaev1404g@mail.ru>
2026-07-14 15:46:47 +0300

    fix

--- tests/base_page_test.py
+++ tests/base_page_test.py
@@ -15,9 +15,9 @@ class TestBasePage:
     # def test_search_base_page(self, base_page):
     #     base_page.fill_search_input()
     #     time.sleep(3)
-    with allure.feature("Login with email"):
-        def test_login_by_email_success(self, base_page, user_data):
-            with allure.step("Open login form"):
-                base_page.open_login()
-            with allure.step("Login with user data"):
-                base_page.login_by_email(email=user_data['email'], password=user_data['password'], expected_name=user_data['expected_name'])
+    @allure.title("Login with email")
+    def test_login_by_email_success(self, base_page, user_data):
+        with allure.step("Open login form"):
+            base_page.open_login()
+        with allure.step("Login with user data"):
+            base_page.login_by_email(email=user_data['email'], password=user_data['password'], expected_name=user_data['expected_name'])
