8ef7036340b84153537dea07b147b3a034ef723e
bugaev <bugaev1404g@mail.ru>
2026-07-14 15:43:56 +0300

    Add allure steps to test

--- tests/base_page_test.py
+++ tests/base_page_test.py
@@ -1,4 +1,6 @@
 import time
+
+import allure
 import pytest
 
 from locators.base_page_locators import BasePageLocators
@@ -10,10 +12,12 @@ from locators.base_page_locators import BasePageLocators
 class TestBasePage:
 
 
-    def test_search_base_page(self, base_page):
-        base_page.fill_search_input()
-        time.sleep(3)
-
-    def test_login_by_email_success(self, base_page, user_data):
-        base_page.open_login()
-        base_page.login_by_email(email=user_data['email'], password=user_data['password'], expected_name=user_data['expected_name'])
+    # def test_search_base_page(self, base_page):
+    #     base_page.fill_search_input()
+    #     time.sleep(3)
+    with allure.feature("Login with email"):
+        def test_login_by_email_success(self, base_page, user_data):
+            with allure.step("Open login form"):
+                base_page.open_login()
+            with allure.step("Login with user data"):
+                base_page.login_by_email(email=user_data['email'], password=user_data['password'], expected_name=user_data['expected_name'])
