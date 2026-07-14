f481a792d51b059870ccc48679067761d6a964c5
bugaev <bugaev1404g@mail.ru>
2026-07-14 17:20:19 +0300

    Add dataclass to tests

--- pages/login_form.py
+++ pages/login_form.py
@@ -1,10 +1,18 @@
 import allure
+from attr import dataclass
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from locators.base_page_locators import BasePageLocators
 from locators.login_form_locators import LoginFormLocators
+
+
+@dataclass
+class ValidUser:
+    email: str
+    password: str
+    expected_name: str
 
 
 class LoginForm:
@@ -19,19 +27,19 @@ class LoginForm:
         self.wait.until(EC.element_to_be_clickable(LoginFormLocators.LOGIN_EMAIL))
 
     @allure.step("Login with email")
-    def login_by_email(self, email, password, expected_name):
+    def login_by_email(self, user:ValidUser):
         with allure.step("Open login form"):
             self.driver.find_element(*LoginFormLocators.LOGIN_EMAIL).click()
-        with allure.step(f"Fill user email with: {email}"):
-            self.wait.until(EC.element_to_be_clickable(LoginFormLocators.EMAIL_INPUT)).send_keys(email)
-        with allure.step(f"Fill user password with: {password}"):
-            self.driver.find_element(*LoginFormLocators.PASSWORD_INPUT).send_keys(password)
+        with allure.step(f"Fill user email with: {user.email}"):
+            self.wait.until(EC.element_to_be_clickable(LoginFormLocators.EMAIL_INPUT)).send_keys(user.email)
+        with allure.step(f"Fill user password with: {user.password}"):
+            self.driver.find_element(*LoginFormLocators.PASSWORD_INPUT).send_keys(user.password)
         with allure.step("Click login button"):
             self.driver.find_element(*LoginFormLocators.LOGIN_SUBMIT_BUTTON).click()
-        with allure.step(f"Verify user {expected_name} is logged in"):
+        with allure.step(f"Verify user {user.expected_name} is logged in"):
             self.wait.until(EC.invisibility_of_element_located(LoginFormLocators.LOGIN_FORM))
 
             profile_name = self.driver.find_element(*BasePageLocators.PROFILE_NAME)
             profile_name = profile_name.text
 
-            assert profile_name == expected_name, f"Ждали: {expected_name}, получили: {profile_name}"
+            assert profile_name == user.expected_name, f"Ждали: {user.expected_name}, получили: {profile_name}"
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,14 +1,17 @@
+import os
+
 import allure
 
-from pages.login_form import LoginForm
+from pages.login_form import LoginForm, ValidUser
 from pages.base_page import BasePage
 
 class TestLogin:
 
     @allure.title("Successful login with email")
-    def test_login_by_email_success(self, driver, base_page, user_data):
+    def test_login_by_email_success(self, driver, base_page):
         login_form = LoginForm(driver)
+        valid_data = ValidUser(email=os.getenv("USER_EMAIL"), password=os.getenv("USER_PASSWORD"), expected_name=os.getenv("EXPECTED_NAME"))
         with allure.step("Open login form"):
             login_form.open_login()
         with allure.step("Login by email"):
-            login_form.login_by_email(email=user_data["email"], password=user_data["password"], expected_name=user_data["expected_name"])
+            login_form.login_by_email(valid_data)
