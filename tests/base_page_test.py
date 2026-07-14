c71c741dd51da87af8a6db6635611848baf85277
bugaev <bugaev1404g@mail.ru>
2026-07-14 16:56:54 +0300

    Refactor: delete login tests from base_page_test.py

--- tests/base_page_test.py
+++ tests/base_page_test.py
@@ -12,12 +12,5 @@ from locators.base_page_locators import BasePageLocators
 class TestBasePage:
 
 
-    # def test_search_base_page(self, base_page):
-    #     base_page.fill_search_input()
-    #     time.sleep(3)
-    @allure.title("Login with email")
-    def test_login_by_email_success(self, base_page, user_data):
-        with allure.step("Open login form"):
-            base_page.open_login()
-        with allure.step("Login with user data"):
-            base_page.login_by_email(email=user_data['email'], password=user_data['password'], expected_name=user_data['expected_name'])
+    def test_search_base_page(self, base_page):
+        base_page.fill_search_input()
