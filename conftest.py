5be1d014f0de5d6f6fdff53721db556e13b0ee98
bugaev <bugaev1404g@mail.ru>
2026-07-14 16:56:32 +0300

    Update conftest.py for local launch

--- conftest.py
+++ conftest.py
@@ -3,13 +3,15 @@ import os
 import pytest
 from dotenv import load_dotenv
 from selenium import webdriver
+
+from pages.login_form import LoginForm
 from utils import attach
 from pages.base_page import BasePage
 
-load_dotenv("test.env")
+# load_dotenv("test.env")
 
 
-# load_dotenv()
+load_dotenv()
 
 
 def pytest_addoption(parser):
@@ -46,38 +48,39 @@ def pytest_addoption(parser):
 
 @pytest.fixture
 def driver(request):
-    browser = request.config.getoption("--browser")
-    browser_version = request.config.getoption("--browser_version")
-    headless = request.config.getoption("--headless").lower() == "true"
-    window_size = request.config.getoption("--window-size")
+    # browser = request.config.getoption("--browser")
+    # browser_version = request.config.getoption("--browser_version")
+    # headless = request.config.getoption("--headless").lower() == "true"
+    # window_size = request.config.getoption("--window-size")
+    #
+    # selenoid_url = os.getenv("SELENOID_URL")
+    # login = os.getenv("LOGIN")
+    # password = os.getenv("PASSWORD")
 
-    selenoid_url = os.getenv("SELENOID_URL")
-    login = os.getenv("LOGIN")
-    password = os.getenv("PASSWORD")
-
-    command_executor = f"https://{login}:{password}@{selenoid_url}"
+    # command_executor = f"https://{login}:{password}@{selenoid_url}"
 
     options = webdriver.ChromeOptions()
+    options.add_argument("--window-size=2560,1440")
+    driver = webdriver.Chrome(options=options)
+    # if headless:
+    #     options.add_argument("--headless")
+    #
+    # options.add_argument(f"--window-size={window_size.replace('x', ',')}")
 
-    if headless:
-        options.add_argument("--headless")
-
-    options.add_argument(f"--window-size={window_size.replace('x', ',')}")
-
-    selenoid_capabilities = {
-        "browserName": browser,
-        "browserVersion": browser_version,
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
-        }
-    }
-    options.capabilities.update(selenoid_capabilities)
-
-    driver = webdriver.Remote(
-        command_executor=command_executor,
-        options=options
-    )
+    # selenoid_capabilities = {
+    #     "browserName": browser,
+    #     "browserVersion": browser_version,
+    #     "selenoid:options": {
+    #         "enableVNC": True,
+    #         "enableVideo": True
+    #     }
+    # }
+    # options.capabilities.update(selenoid_capabilities)
+    #
+    # driver = webdriver.Remote(
+    #     command_executor=command_executor,
+    #     options=options
+    # )
 
     yield driver
 
