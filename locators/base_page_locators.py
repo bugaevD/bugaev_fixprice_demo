d928e964cd7fae73038e8e2c281ac1a2c08c90b0
bugaev <bugaev1404g@mail.ru>
2026-07-15 18:21:58 +0300

    Delete locators pages

--- locators/base_page_locators.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class BasePageLocators:
-
-    MAIN_PAGE_LOGO = (By.CSS_SELECTOR, ".logo")
-    SEARCH_INPUT = (By.CSS_SELECTOR, ".search-input input")
-    PROFILE_NAME = (By.CSS_SELECTOR, ".login-btn .profile")
-    PROFILE_MENU = (By.CSS_SELECTOR, ".subcategories-container.new-profile")
--- locators/login_form_locators.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class LoginFormLocators:
-    LOGIN_FORM = (By.CSS_SELECTOR, ".wrapper.modal-child")
-    LOGIN_BUTTON = (By.CSS_SELECTOR, ".login-btn")
-    LOGIN_EMAIL = (By.XPATH, "//button[contains(@class, 'outline-tetriary') and span[text()='По email']]")
-    EMAIL_INPUT = (By.CSS_SELECTOR, "[data-test='input'][type='email']")
-    PASSWORD_INPUT = (By.CSS_SELECTOR, "[data-test='input'][type='password']")
-    LOGIN_SUBMIT_BUTTON = (By.XPATH, "//button[contains(@class, 'button') and span[text()='Войти']]")
