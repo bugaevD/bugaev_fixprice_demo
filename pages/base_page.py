7ebc203394667400ab62b5acb7eae9fca8cf4b95
bugaev <bugaev1404g@mail.ru>
2026-07-15 18:21:43 +0300

    Move locators to classes

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,16 @@
 import allure
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from locators.base_page_locators import BasePageLocators
 from selenium.webdriver.support import expected_conditions as EC
 
 
 class BasePage:
+
+    MAIN_PAGE_LOGO = (By.CSS_SELECTOR, ".logo")
+    SEARCH_INPUT = (By.CSS_SELECTOR, ".search-input input")
+    PROFILE_NAME = (By.CSS_SELECTOR, ".login-btn .profile")
+    PROFILE_MENU = (By.CSS_SELECTOR, ".subcategories-container.new-profile")
 
     def __init__(self, driver):
         self.driver = driver
@@ -13,16 +19,16 @@ class BasePage:
     @allure.step("Open main page")
     def open_base_page(self, url):
         self.driver.get(url)
-        self.wait.until(EC.visibility_of_element_located(BasePageLocators.MAIN_PAGE_LOGO))
+        self.wait.until(EC.visibility_of_element_located(self.MAIN_PAGE_LOGO))
 
     def get_profile_name(self):
-        return self.driver.find_element(*BasePageLocators.PROFILE_NAME).text
+        return self.driver.find_element(*self.PROFILE_NAME).text
 
     def select_city(self, city):
         pass
 
     def fill_search_input(self, text="булдак"):
-        search_input = self.driver.find_element(*BasePageLocators.SEARCH_INPUT)
+        search_input = self.driver.find_element(*self.SEARCH_INPUT)
         search_input.click()
         search_input.clear()
         search_input.send_keys(text)
--- pages/login_form.py
+++ pages/login_form.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from locators.base_page_locators import BasePageLocators
 from locators.login_form_locators import LoginFormLocators
+from pages.base_page import BasePage
 
 
 @dataclass
@@ -16,6 +17,12 @@ class ValidUser:
 
 
 class LoginForm:
+    LOGIN_FORM = (By.CSS_SELECTOR, ".wrapper.modal-child")
+    LOGIN_BUTTON = (By.CSS_SELECTOR, ".login-btn")
+    LOGIN_EMAIL = (By.XPATH, "//button[contains(@class, 'outline-tetriary') and span[text()='По email']]")
+    EMAIL_INPUT = (By.CSS_SELECTOR, "[data-test='input'][type='email']")
+    PASSWORD_INPUT = (By.CSS_SELECTOR, "[data-test='input'][type='password']")
+    LOGIN_SUBMIT_BUTTON = (By.XPATH, "//button[contains(@class, 'button') and span[text()='Войти']]")
 
     def __init__(self, driver):
         self.driver = driver
@@ -23,23 +30,23 @@ class LoginForm:
 
     @allure.step("Open login form")
     def open_login(self):
-        self.driver.find_element(*LoginFormLocators.LOGIN_BUTTON).click()
-        self.wait.until(EC.element_to_be_clickable(LoginFormLocators.LOGIN_EMAIL))
+        self.driver.find_element(*self.LOGIN_BUTTON).click()
+        self.wait.until(EC.element_to_be_clickable(self.LOGIN_EMAIL))
 
     @allure.step("Login with email")
-    def login_by_email(self, user:ValidUser):
+    def login_by_email(self, user: ValidUser):
         with allure.step("Open login form"):
-            self.driver.find_element(*LoginFormLocators.LOGIN_EMAIL).click()
+            self.driver.find_element(*self.LOGIN_EMAIL).click()
         with allure.step(f"Fill user email with: {user.email}"):
-            self.wait.until(EC.element_to_be_clickable(LoginFormLocators.EMAIL_INPUT)).send_keys(user.email)
+            self.wait.until(EC.element_to_be_clickable(self.EMAIL_INPUT)).send_keys(user.email)
         with allure.step(f"Fill user password with: {user.password}"):
-            self.driver.find_element(*LoginFormLocators.PASSWORD_INPUT).send_keys(user.password)
+            self.driver.find_element(*self.PASSWORD_INPUT).send_keys(user.password)
         with allure.step("Click login button"):
-            self.driver.find_element(*LoginFormLocators.LOGIN_SUBMIT_BUTTON).click()
+            self.driver.find_element(*self.LOGIN_SUBMIT_BUTTON).click()
         with allure.step(f"Verify user {user.expected_name} is logged in"):
-            self.wait.until(EC.invisibility_of_element_located(LoginFormLocators.LOGIN_FORM))
+            self.wait.until(EC.invisibility_of_element_located(self.LOGIN_FORM))
 
-            profile_name = self.driver.find_element(*BasePageLocators.PROFILE_NAME)
+            profile_name = self.driver.find_element(*BasePage.PROFILE_NAME)
             profile_name = profile_name.text
 
             assert profile_name == user.expected_name, f"Ждали: {user.expected_name}, получили: {profile_name}"
